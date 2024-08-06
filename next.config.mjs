c087a1132ca1271b2ed5ff90c396c5fe41c5819a
Hung Nguyen <nguyen.phathung123@gmail.com>
2024-08-06 12:39:21 +0700

    Fix "images.domains" configuration is deprecated

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['firebasestorage.googleapis.com'],
-      },
-      webpack(config) {
-        config.module.rules.push({
-          test: /\.svg$/,
-          use: ['@svgr/webpack'],
-        });
-    
-        return config;
-      },
+  images: {
+      remotePatterns: [
+          {
+              protocol: 'https',
+              hostname: 'firebasestorage.googleapis.com',
+          },
+      ],
+  },
+  webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
+
+      return config;
+  },
 };
 
 export default nextConfig;
